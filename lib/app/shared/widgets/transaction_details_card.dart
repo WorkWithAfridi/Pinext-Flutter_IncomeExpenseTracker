29fca7bd8f04518c16f229c866e2555d4a9b56a6
Khondakar Afridi <khondakarafridi35@gmail.com>
2022-12-24 10:19:39 +0600

    Clicking on any part of the card now triggers event

--- lib/app/shared/widgets/transaction_details_card.dart
+++ lib/app/shared/widgets/transaction_details_card.dart
@@ -46,113 +46,116 @@ class _TransactionDetailsCardState extends State<TransactionDetailsCard> {
           )),
         );
       },
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 8,
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                DateFormat('dd-MM-yyyy').format(DateTime.parse(widget.pinextTransactionModel.transactionDate)),
-                style: regularTextStyle.copyWith(
-                  color: customBlackColor.withOpacity(.80),
+      child: Container(
+        color: Colors.transparent,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 8,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  DateFormat('dd-MM-yyyy').format(DateTime.parse(widget.pinextTransactionModel.transactionDate)),
+                  style: regularTextStyle.copyWith(
+                    color: customBlackColor.withOpacity(.80),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                width: 15,
-              ),
-              Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Flexible(
-                      flex: 2,
-                      child: FutureBuilder(
-                        initialData: "",
-                        future: getCardTitle(),
-                        builder: ((context, snapshot) {
-                          return Text(
-                            snapshot.data.toString(),
-                            style: regularTextStyle.copyWith(
-                              color: customBlackColor.withOpacity(.80),
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          );
-                        }),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        flex: 2,
+                        child: FutureBuilder(
+                          initialData: "",
+                          future: getCardTitle(),
+                          builder: ((context, snapshot) {
+                            return Text(
+                              snapshot.data.toString(),
+                              style: regularTextStyle.copyWith(
+                                color: customBlackColor.withOpacity(.80),
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            );
+                          }),
+                        ),
                       ),
-                    ),
-                    Text(
-                      " - ",
-                      style: regularTextStyle.copyWith(
-                        color: customBlackColor.withOpacity(.80),
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Flexible(
-                      flex: 3,
-                      child: Text(
-                        widget.pinextTransactionModel.details,
+                      Text(
+                        " - ",
                         style: regularTextStyle.copyWith(
                           color: customBlackColor.withOpacity(.80),
                         ),
-                        maxLines: 2,
+                        maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Container(
-                // width: 100,
-                alignment: Alignment.centerRight,
-                child: Text(
-                  widget.pinextTransactionModel.transactionType == 'Expense'
-                      ? "- ${widget.pinextTransactionModel.amount}Tk"
-                      : "+ ${widget.pinextTransactionModel.amount}Tk",
-                  style: boldTextStyle.copyWith(
-                    color: widget.pinextTransactionModel.transactionType == 'Expense' ? Colors.red : Colors.green,
+                      Flexible(
+                        flex: 3,
+                        child: Text(
+                          widget.pinextTransactionModel.details,
+                          style: regularTextStyle.copyWith(
+                            color: customBlackColor.withOpacity(.80),
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              // const SizedBox(
-              //   width: 8,
-              // ),
-              // GestureDetector(
-              //   onTap: () {
-              //     Navigator.push(
-              //       context,
-              //       CustomTransitionPageRoute(
-              //           childWidget: AddAndEditTransactionScreen(
-              //         isEdit: true,
-              //         isAQuickAction: false,
-              //         pinextTransactionModel: pinextTransactionModel,
-              //       )),
-              //     );
-              //   },
-              //   child: const Icon(
-              //     Icons.edit,
-              //     color: customBlueColor,
-              //     size: 16,
-              //   ),
-              // )
-            ],
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Container(
-            height: 1,
-            width: getWidth(context),
-            color: customBlackColor.withOpacity(.05),
-          )
-        ],
+                const SizedBox(
+                  width: 8,
+                ),
+                Container(
+                  // width: 100,
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    widget.pinextTransactionModel.transactionType == 'Expense'
+                        ? "- ${widget.pinextTransactionModel.amount}Tk"
+                        : "+ ${widget.pinextTransactionModel.amount}Tk",
+                    style: boldTextStyle.copyWith(
+                      color: widget.pinextTransactionModel.transactionType == 'Expense' ? Colors.red : Colors.green,
+                    ),
+                  ),
+                ),
+                // const SizedBox(
+                //   width: 8,
+                // ),
+                // GestureDetector(
+                //   onTap: () {
+                //     Navigator.push(
+                //       context,
+                //       CustomTransitionPageRoute(
+                //           childWidget: AddAndEditTransactionScreen(
+                //         isEdit: true,
+                //         isAQuickAction: false,
+                //         pinextTransactionModel: pinextTransactionModel,
+                //       )),
+                //     );
+                //   },
+                //   child: const Icon(
+                //     Icons.edit,
+                //     color: customBlueColor,
+                //     size: 16,
+                //   ),
+                // )
+              ],
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Container(
+              height: 1,
+              width: getWidth(context),
+              color: customBlackColor.withOpacity(.05),
+            )
+          ],
+        ),
       ),
     );
   }
