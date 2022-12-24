5abcbacee61e0d0f23aa53466745d4b10fba8641
Khondakar Afridi <khondakarafridi35@gmail.com>
2022-12-24 10:17:17 +0600

    Added wallet title in transaction card

--- lib/app/shared/widgets/transaction_details_card.dart
+++ lib/app/shared/widgets/transaction_details_card.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:pinext/app/models/pinext_card_model.dart';
+import 'package:pinext/app/services/firebase_services.dart';
+import 'package:pinext/app/services/handlers/user_handler.dart';
 
 import '../../app_data/app_constants/domentions.dart';
 import '../../app_data/app_constants/fonts.dart';
@@ -8,13 +12,25 @@ import '../../app_data/theme_data/colors.dart';
 import '../../models/pinext_transaction_model.dart';
 import '../../screens/add_and_view_transaction/add_and_view_transaction.dart';
 
-class TransactionDetailsCard extends StatelessWidget {
+class TransactionDetailsCard extends StatefulWidget {
   const TransactionDetailsCard({
     Key? key,
     required this.pinextTransactionModel,
   }) : super(key: key);
 
   final PinextTransactionModel pinextTransactionModel;
+
+  @override
+  State<TransactionDetailsCard> createState() => _TransactionDetailsCardState();
+}
+
+class _TransactionDetailsCardState extends State<TransactionDetailsCard> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCardDetails();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +42,7 @@ class TransactionDetailsCard extends StatelessWidget {
               childWidget: AddAndViewTransactionScreen(
             isViewOnly: true,
             isAQuickAction: false,
-            pinextTransactionModel: pinextTransactionModel,
+            pinextTransactionModel: widget.pinextTransactionModel,
           )),
         );
       },
@@ -36,38 +52,72 @@ class TransactionDetailsCard extends StatelessWidget {
             height: 8,
           ),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                DateFormat('dd-MM-yyyy').format(DateTime.parse(pinextTransactionModel.transactionDate)),
+                DateFormat('dd-MM-yyyy').format(DateTime.parse(widget.pinextTransactionModel.transactionDate)),
                 style: regularTextStyle.copyWith(
                   color: customBlackColor.withOpacity(.80),
                 ),
               ),
               const SizedBox(
-                width: 32,
+                width: 15,
               ),
               Expanded(
-                child: Text(
-                  pinextTransactionModel.details,
-                  style: regularTextStyle.copyWith(
-                    color: customBlackColor.withOpacity(.80),
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: FutureBuilder(
+                        initialData: "",
+                        future: getCardTitle(),
+                        builder: ((context, snapshot) {
+                          return Text(
+                            snapshot.data.toString(),
+                            style: regularTextStyle.copyWith(
+                              color: customBlackColor.withOpacity(.80),
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          );
+                        }),
+                      ),
+                    ),
+                    Text(
+                      " - ",
+                      style: regularTextStyle.copyWith(
+                        color: customBlackColor.withOpacity(.80),
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Flexible(
+                      flex: 3,
+                      child: Text(
+                        widget.pinextTransactionModel.details,
+                        style: regularTextStyle.copyWith(
+                          color: customBlackColor.withOpacity(.80),
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
                 ),
               ),
               const SizedBox(
                 width: 8,
               ),
               Container(
-                width: 100,
+                // width: 100,
                 alignment: Alignment.centerRight,
                 child: Text(
-                  pinextTransactionModel.transactionType == 'Expense'
-                      ? "- ${pinextTransactionModel.amount}Tk"
-                      : "+ ${pinextTransactionModel.amount}Tk",
+                  widget.pinextTransactionModel.transactionType == 'Expense'
+                      ? "- ${widget.pinextTransactionModel.amount}Tk"
+                      : "+ ${widget.pinextTransactionModel.amount}Tk",
                   style: boldTextStyle.copyWith(
-                    color: pinextTransactionModel.transactionType == 'Expense' ? Colors.red : Colors.green,
+                    color: widget.pinextTransactionModel.transactionType == 'Expense' ? Colors.red : Colors.green,
                   ),
                 ),
               ),
@@ -105,5 +155,23 @@ class TransactionDetailsCard extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<PinextCardModel> getCardDetails() async {
+    String cardId = widget.pinextTransactionModel.cardId;
+    DocumentSnapshot cardData = await FirebaseServices()
+        .firebaseFirestore
+        .collection("pinext_users")
+        .doc(UserHandler().currentUser.userId)
+        .collection("pinext_cards")
+        .doc(cardId)
+        .get();
+
+    return PinextCardModel.fromMap(cardData.data() as Map<String, dynamic>);
+  }
+
+  Future<String> getCardTitle() async {
+    PinextCardModel card = await getCardDetails();
+    return card.title;
   }
 }
