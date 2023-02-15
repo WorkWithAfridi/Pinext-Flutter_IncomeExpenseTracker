1b1f5554bb8d170bf0908fd0e1dd06cf11963de8
afridi <afridi@tikweb.com>
2023-02-15 15:52:04 +0600

    budget section : wip

---
+++ lib/app/models/pinext_subscription_model.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+class PinextSubscriptionModel {
+  String dateAdded;
+  String lastPaidOn;
+  String amount;
+  String subscriptionId;
+  String assignedCardId;
+  String automaticallyDeductEnabled;
+  String description;
+  String title;
+  PinextSubscriptionModel({
+    required this.dateAdded,
+    required this.lastPaidOn,
+    required this.amount,
+    required this.subscriptionId,
+    required this.assignedCardId,
+    required this.automaticallyDeductEnabled,
+    required this.description,
+    required this.title,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'dateAdded': dateAdded,
+      'lastPaidOn': lastPaidOn,
+      'amount': amount,
+      'subscriptionId': subscriptionId,
+      'assignedCardId': assignedCardId,
+      'automaticallyDeductEnabled': automaticallyDeductEnabled,
+      'description': description,
+      'title': title,
+    };
+  }
+
+  factory PinextSubscriptionModel.fromMap(Map<String, dynamic> map) {
+    return PinextSubscriptionModel(
+      dateAdded: map['dateAdded'] as String,
+      lastPaidOn: map['lastPaidOn'] as String,
+      amount: map['amount'] as String,
+      subscriptionId: map['subscriptionId'] as String,
+      assignedCardId: map['assignedCardId'] as String,
+      automaticallyDeductEnabled: map['automaticallyDeductEnabled'] as String,
+      description: map['description'] as String,
+      title: map['title'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory PinextSubscriptionModel.fromJson(String source) =>
+      PinextSubscriptionModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'PinextSubscriptionModel(dateAdded: $dateAdded, lastPaidOn: $lastPaidOn, amount: $amount, subscriptionId: $subscriptionId, assignedCardId: $assignedCardId, automaticallyDeductEnabled: $automaticallyDeductEnabled, description: $description, title: $title)';
+  }
+}
--- lib/app/screens/home/pages/budget_pages/add_subscription_page.dart
+++ lib/app/screens/home/pages/budget_pages/add_subscription_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pinext/app/app_data/app_constants/constants.dart';
 import 'package:pinext/app/app_data/app_constants/fonts.dart';
+import 'package:pinext/app/app_data/extensions/string_extensions.dart';
 import 'package:pinext/app/app_data/theme_data/colors.dart';
+import 'package:pinext/app/shared/widgets/custom_text_field.dart';
+import 'package:pinext/app/shared/widgets/info_widget.dart';
 
 class AddSubscriptionPage extends StatelessWidget {
   AddSubscriptionPage({
@@ -35,16 +38,127 @@ class AddSubscriptionPage extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: defaultPadding,
-            ),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [],
-              ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: defaultPadding,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        "Subscription title",
+                        style: boldTextStyle,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      CustomTextFormField(
+                        controller: TextEditingController(),
+                        hintTitle: "Netflix subscription",
+                        textInputType: TextInputType.number,
+                        onChanged: (String value) {},
+                        validator: (String value) {
+                          return InputValidation(value).isNotEmpty();
+                        },
+                        suffixButtonAction: () {},
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        "Subscription description",
+                        style: boldTextStyle,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      CustomTextFormField(
+                        numberOfLines: 4,
+                        controller: TextEditingController(),
+                        hintTitle: "",
+                        textInputType: TextInputType.number,
+                        onChanged: (String value) {},
+                        validator: (String value) {
+                          return null;
+                        },
+                        suffixButtonAction: () {},
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        "Subscription amount",
+                        style: boldTextStyle,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      CustomTextFormField(
+                        numberOfLines: 1,
+                        controller: TextEditingController(),
+                        hintTitle: "",
+                        textInputType: TextInputType.number,
+                        onChanged: (String value) {},
+                        validator: (String value) {
+                          return InputValidation(value).isCorrectNumber();
+                        },
+                        suffixButtonAction: () {},
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 8,
+                    left: defaultPadding,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Automatically pay",
+                        style: boldTextStyle,
+                      ),
+                      Switch(
+                        value: true,
+                        activeColor: customBlueColor,
+                        onChanged: (value) {},
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: defaultPadding,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      InfoWidget(
+                        infoText:
+                            "Enabling this option will automatically deduct the subscription amount at the start of everymonth, from the selected card!",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
