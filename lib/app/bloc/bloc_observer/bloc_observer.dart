229e045e8b9cf548245bdc9e5cd5942f727add1f
Khondakar Afridi <afridi@tikweb.com>
2023-03-29 15:39:21 +0600

    Code refactoring

--- lib/app/bloc/bloc_observer/bloc_observer.dart
+++ lib/app/bloc/bloc_observer/bloc_observer.dart
@@ -4,25 +4,25 @@ import 'package:bloc/bloc.dart';
 
 class PinextObserver extends BlocObserver {
   @override
-  void onCreate(BlocBase bloc) {
+  void onCreate(BlocBase<dynamic> bloc) {
     super.onCreate(bloc);
     log('onCreate -- ${bloc.runtimeType}');
   }
 
   @override
-  void onChange(BlocBase bloc, Change change) {
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
     log('onChange -- ${bloc.runtimeType}, $change');
   }
 
   @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     log('onError -- ${bloc.runtimeType}, $error');
     super.onError(bloc, error, stackTrace);
   }
 
   @override
-  void onClose(BlocBase bloc) {
+  void onClose(BlocBase<dynamic> bloc) {
     super.onClose(bloc);
     log('onClose -- ${bloc.runtimeType}');
   }
