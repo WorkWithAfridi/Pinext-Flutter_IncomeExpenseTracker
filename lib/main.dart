d4fe7465584f8545d3975fc0d55c500086da0da3
KhondakarAfridi <afridi.khondakar@gmail.com>
2023-05-15 08:49:40 +0600

    3.0.3 going live

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,9 @@ void main(List<String> args) async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // Pass all uncaught "fatal" errors from the framework to Crashlytics
   FlutterError.onError = (errorDetails) {
     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
   };
-  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
   PlatformDispatcher.instance.onError = (error, stack) {
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
     return true;
