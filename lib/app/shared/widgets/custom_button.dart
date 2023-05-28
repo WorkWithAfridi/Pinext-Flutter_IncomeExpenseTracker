374fbb967cce95a83936d8141f9b7742d1d07597
KhondakarAfridi <afridi.khondakar@gmail.com>
2023-05-28 21:10:42 +0600

    Updated button loading style

--- lib/app/shared/widgets/custom_button.dart
+++ lib/app/shared/widgets/custom_button.dart
@@ -87,6 +87,7 @@ class ButtonLoadingAnimation extends StatelessWidget {
       width: 30,
       child: const CircularProgressIndicator(
         color: Colors.white,
+        strokeWidth: 1,
       ),
 
       // LottieBuilder.asset(
